9574ec59819caea4771f3e221fc4e22e0929959c
Vincent Letourneau <vincent@nanoninja.com>
2018-11-30 14:05:17 +0100

    Add Task

--- js/jquery/main.js
+++ js/jquery/main.js
@@ -1,13 +1,22 @@
-
 $(document).ready(function () {
-    let text = '';
+    $('.tasks').on('click', function (event) {
+        console.log(event);
+    });
 
-    if (text.trim()) {
-        
-    }
+    $('.textTask').on('keyup', function (event) {
+        if (event.keyCode === 13) {
+            if ($(this).val().trim()) {
+                var tmpl =
+                    '<li>' +
+                        '<a href="#" class="todo-checkbox"><i class="fa fa-check-square"></i></a>' +
+                        '<span class="todo-text">' + $(this).val() + '</span>' +
+                        '<a href="#"><i class="todo-trash fa fa-trash"></i></a>' +
+                    '</li>';
 
-    $('#textTask').on('keyup', function (event) {
-        $('#textDisplay').text($(this).val());
+                $('.tasks').append(tmpl);
+                $(this).val('');
+            }
+        }
     });
 
 });
